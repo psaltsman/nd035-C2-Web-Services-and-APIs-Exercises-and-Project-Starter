fc701418411fb9ec3facf6a4486839b5d1906f78
Paul Saltsman <paul.c.saltsman@gmail.com>
2021-05-04 10:48:35 -0400

    Adds eureka server to project and implements tests for pricing service

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -3,6 +3,12 @@ package com.udacity.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
@@ -14,4 +20,10 @@ public class PricingServiceApplication {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
 
+    @Bean
+    @Qualifier("restTemplate")
+    public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+
+        return builder.build();
+    }
 }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -5,12 +5,36 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.*;
+
+import com.udacity.pricing.domain.price.Price;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PricingServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	RestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getPricingService() {
+
+		ResponseEntity<Price> response = restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+
+		assertThat(response.getStatusCode(),equalTo(HttpStatus.OK));
+	}
 }
