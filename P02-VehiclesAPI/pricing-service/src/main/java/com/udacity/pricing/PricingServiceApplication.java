b87bddf40a680d489feb2aa238360e928f50785d
Paul Saltsman <paul.c.saltsman@gmail.com>
2021-05-05 09:46:41 -0400

    Adds annotation for enabling the eureka client

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -9,10 +9,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
